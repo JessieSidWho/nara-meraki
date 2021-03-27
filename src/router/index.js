ad899039b350efaca201f65ebefbd83860cfe946
Jessie Sidhu <jsidhu22@gmail.com>
2021-03-26 18:45:01 -0700

    added hashrouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -12,20 +12,18 @@ const Router = () => {
     <Suspense fallback={null}>
       <GlobalStyles />
       <Header />
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          {routes.map((routeItem) => {
-            return (
-              <Route
-                key={routeItem.component}
-                path={routeItem.path}
-                exact={routeItem.exact}
-                component={lazy(() => import(`../pages/${routeItem.component}`))}
-              />
-            );
-          })}
-        </Switch>
-      </Router>
+      <Switch>
+        {routes.map((routeItem) => {
+          return (
+            <Route
+              key={routeItem.component}
+              path={routeItem.path}
+              exact={routeItem.exact}
+              component={lazy(() => import(`../pages/${routeItem.component}`))}
+            />
+          );
+        })}
+      </Switch>
       <Footer />
     </Suspense>
   );
